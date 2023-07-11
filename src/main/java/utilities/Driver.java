dab74e6010b71d5d13b26e194583d05d50190fa8
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-11 22:02:40 +0300

    baseproject

---
+++ src/main/java/utilities/Driver.java
@@ -0,0 +1,44 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+
+    /**
+     * Bu method overload metodu kullanarak default chrome driver return etmeli
+     * @return webdriver return etmeli
+     */
+    public WebDriver getDriver(){
+        //TODO kodlarını buraya yazınız
+        return null;
+    }
+
+    /**
+     * browser parametresi ile gelen driver'i create etmeli
+     * @param browser chrome, firefox, edge, ... olmali
+     * @return webdriver return etmeli
+     */
+    public WebDriver getDriver(String browser){
+        //TODO kodlarinizi buraya yaziniz
+        return null;
+    }
+
+    /**
+     * driver'i quit eden metod
+     */
+    public void quitDriver(){
+        //TODO kodlarinizi buraya yaziniz
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
