f3847166777a1a592975bae3973abd8fabea25ef
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-12 12:40:00 +0300

    locators

---
+++ src/test/java/locators/LocatorsDemoqa.java
@@ -0,0 +1,16 @@
+package locators;
+
+import org.openqa.selenium.By;
+
+public interface LocatorsDemoqa {
+    //Demoqa'e ait locator'lar bu interface'de olmalı.
+
+
+    By xx = By.id("");
+    By yy = By.id("");
+
+
+
+
+
+}
