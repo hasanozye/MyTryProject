160d391e1b5f0ebe877d8a0cc61d34b5dadd8fb8
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-14 10:40:16 +0300

    Added OpenCart locators interface with element locators for My Account, Register, First Name, Last Name, Email, Telephone, Password, Password Confirmation, Privacy Policy, Continue Button, Congratulations Page, Logout, and Logout Page.

--- src/test/java/locators/LocatorsOpencart.java
+++ src/test/java/locators/LocatorsOpencart.java
@@ -1,4 +1,24 @@
 package locators;
 
+import org.openqa.selenium.By;
+
 public interface LocatorsOpencart {
+
+    By lMyAccountO = By.xpath("//a[@title='My Account']");
+    By lRegisterO= By.xpath("//*[text()='Register']");
+    By lFirstNameO = By.cssSelector("#input-firstname");
+    By lLastNameO = By.cssSelector("#input-lastname");
+    By lEMailO = By.cssSelector("#input-email");
+    By lTelephoneO = By.cssSelector("#input-telephone");
+    By lPasswordO = By.cssSelector("#input-password");
+    By lPasswordConfirmO = By.cssSelector("#input-confirm");
+    By lPrivPolicyO = By.xpath("//*[@name='agree']");
+    By lContinueBtnO = By.xpath("//*[@value='Continue']");
+    By lCongratsPageO = By.xpath("//*[text()='Congratulations! Your new account has been successfully created!']");
+    By lLogOutO = By.xpath("//li//a[text()='Logout']");
+    By lLogoutPage0 = By.xpath("//*[text()='You have been logged off your account. It is now safe to leave the computer.']");
+
+
+
+
 }
